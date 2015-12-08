7971dfb57b3908cf93854986100eca688f9d0ffd
SokolnikovVV <xyxb@inbox.ru>
2015-12-08 16:36:29 +0300

    Там это, вон это, как его

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Привет как дела");
+        primaryStage.setScene(new Scene(root, 20, 800));
         primaryStage.show();
     }
 
